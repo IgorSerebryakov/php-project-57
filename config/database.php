18f69abee19f6a4f52a53654d767ee333cadc14c
IgorSerebryakov <serebryakov09@mail.ru>
2024-07-19 17:20:38 +0300

    refactor config/database.php

--- config/database.php
+++ config/database.php
@@ -85,10 +85,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dpg-cqclgq56l47c73d7b74g-a'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'db_php_project_57'),
+            'username' => env('DB_USERNAME', 'xen'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
