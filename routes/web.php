320d6f8151d703d8d02d834e5c33dfaa25db0ded
IgorSerebryakov <serebryakov09@mail.ru>
2024-07-21 12:03:36 +0300

    + added gmail-config

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*  middleware(['auth', 'verified']) - добавляет обработчики auth и verified к маршруту.
+ *  auth - только аутентифицированные пользователи могут получить доступ к маршруту. Если пользователь не вошёл
+ *  в систему, он будет перенаправлен на страницу входа.
+ *  verified - подтвердил ли пользователь свой адрес электронной почты. Ограничивает доступ к маршруту для пользователей,
+ *  чья электронная почта не была подтверждена.
+ */
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+/*
+ *
+ *
+ */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
