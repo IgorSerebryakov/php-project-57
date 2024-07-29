d5ec61aef01b267505247d4bf253eeb0b4ace061
IgorSerebryakov <serebryakov09@mail.ru>
2024-07-29 19:49:27 +0300

    finished TaskStatusControllerTest

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -13,27 +13,51 @@ use Illuminate\Support\Facades\DB;
 use Laracasts\Flash\FlashNotifier;
 use Tests\TestCase;
 
-// Создать БД для тестирования
-
 class TaskStatusControllerTest extends TestCase
 {
     use RefreshDatabase;
-    public function testDestroy(): void
+
+    private TaskStatus $taskStatus;
+    private TaskStatus $newTaskStatus;
+    private User $user;
+
+    protected function setUp(): void
     {
-        $taskStatus = TaskStatus::query()->create([
-            'name' => fake()->name()
-        ]);
-        $response = $this->delete(route('task_statuses.destroy', $taskStatus->id));
+        parent::setUp();
+
+        $this->taskStatus = TaskStatus::factory()->create();
+        $this->newTaskStatus = TaskStatus::factory()->make();
+        $this->user = User::factory()->make();
+    }
+
+
+
+    public function testCreateNotAllowedForGuest()
+    {
+        $response = $this->get(route('task_statuses.create'));
+        $response->assertStatus(403);
+    }
+
+    public function testUpdateNotAllowedForGuest()
+    {
+        $data = ['name' => 'TaskStatusForUpdateTest'];
+        $response = $this->patch(route('task_statuses.update', $this->taskStatus->id), $data);
+        $this->assertDatabaseHas('task_statuses', $this->taskStatus->toArray());
+        $response->assertStatus(403);
+    }
+
+    public function testUpdate()
+    {
+        $response = $this->actingAs($this->user)
+            ->patch(route('task_statuses.update', $this->taskStatus->id), $this->newTaskStatus->toArray());
+        $response->assertSessionHasNoErrors();
         $response->assertRedirect(route('task_statuses.index'));
-        $flashMessage = session('flash_notification')[0]['message'];
-        $this->assertDatabaseMissing('task_statuses', ['id' => $taskStatus->id]);
-        $this->assertEquals($flashMessage, __('flash.task_status.destroy.success'));
+        $this->assertDatabaseHas('task_statuses', $this->newTaskStatus->toArray());
     }
 
     public function testNotDestroy(): void
     {
         $this->seed([TaskStatusSeeder::class]);
-        $this->assertDatabaseCount('task_statuses', 4);
         DB::table('task_statuses')
             ->whereIn('name', [
                 __('seeders.task_status.new'),
@@ -43,38 +67,24 @@ class TaskStatusControllerTest extends TestCase
             ])
             ->get()
             ->each(function ($taskStatus) {
-                $response = $this->delete(route('task_statuses.destroy', $taskStatus->id));
+                $response = $this->actingAs($this->user)->delete(route('task_statuses.destroy', $taskStatus->id));
                 $response->assertRedirect(route('task_statuses.index'));
-                $flashMessage = session('flash_notification')[0]['message'];
                 $this->assertDatabaseHas('task_statuses', ['id' => $taskStatus->id]);
-                $this->assertEquals($flashMessage, __('flash.task_status.destroy.fail'));
+                $this->assertFlashMessage(__('flash.task_status.destroy.fail'));
             });
     }
 
-    public function testCreateNotAllowedForGuest()
+    public function testDestroy(): void
     {
-        $response = $this->get(route('task_statuses.create'));
-        $response->assertStatus(403);
+        $response = $this->actingAs($this->user)->delete(route('task_statuses.destroy', $this->taskStatus->id));
+        $response->assertRedirect(route('task_statuses.index'));
+        $this->assertDatabaseMissing('task_statuses', ['id' => $this->taskStatus->id]);
+        $this->assertFlashMessage(__('flash.task_status.destroy.success'));
     }
 
-    public function testStore()
+    protected function assertFlashMessage($expectedMessage)
     {
-        $randomName = fake()->name();
-        $this->actingAs(User::factory()->create())
-            ->post(route('task_statuses.store'), ['name' => $randomName]);
-        $this->assertDatabaseHas('task_statuses', ['name' => $randomName]);
-    }
-
-    public function testUpdateNotAllowedForGuest()
-    {
-        $oldTaskStatus = TaskStatus::query()->create([
-            'name' => fake()->name()
-        ]);
-        $data = [
-            'name' => 'TaskStatusForUpdateTest'
-        ];
-        $response = $this->patch(route('task_statuses.update', $oldTaskStatus->id), $data);
-        $this->assertDatabaseHas('task_statuses', $oldTaskStatus->toArray());
-        $response->assertStatus(403);
+        $flashMessage = session('flash_notification')[0]['message'];
+        $this->assertEquals($expectedMessage, $flashMessage);
     }
 }
