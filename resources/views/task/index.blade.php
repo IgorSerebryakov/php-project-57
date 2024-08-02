7a855abcecba214b973cb685fce6121fbfbcbc73
IgorSerebryakov <serebryakov09@mail.ru>
2024-08-02 21:45:00 +0300

    + refactor delete method

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -8,7 +8,7 @@
         @auth
             <a href="{{ route('tasks.edit', $task->id) }}">Изменить</a>
             @if ($task->creator->id === \Illuminate\Support\Facades\Auth::id())
-                <a href="{{ route('tasks.destroy', $task->id) }}">Удалить</a>
+                <a href="{{ route('tasks.destroy', $task->id) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
             @endif
         @endauth
     @endforeach
